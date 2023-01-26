fbd78e09de00d854b3ed0f56eac390f5c2e15bfa
SamueleLettieri <samuele2073@gmail.com>
2023-01-26 15:53:06 +0100

    esercizio cibi preferiti

---
+++ src/org/lessons/java/security/CibiPreferiti.java
@@ -0,0 +1,15 @@
+package org.lessons.java.security;
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+		String[] favoriteFoods = {"Pizza", "Lasagna", "Pollo", "Pasta al forno", "Torttelini"};
+		
+		System.out.println(favoriteFoods.length);
+		
+		System.out.println("il mio cibbo preferito è: " + favoriteFoods[0]);
+		
+		System.out.println("il mio cibbo meno preferito è: " + favoriteFoods[4]);
+	}
+	
+}
